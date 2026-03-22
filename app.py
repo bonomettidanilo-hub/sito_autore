14d6c68fa38e7b6f77c376a3a391011cd36923f2
MacBook nuovo Dad <macbooknuovodad@MacBook-Air-di-MacBook.local>
2026-03-22 18:17:39 +0100

    step 1 base sito

--- app.py
+++ app.py
@@ -4,9 +4,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>SITO FUNZIONA</h1>"
+    return """
+    <html>
+    <head>
+        <title>Danilo Bonometti</title>
+        <style>
+            body {
+                margin:0;
+                font-family: Arial;
+                background:#000;
+                color:white;
+                text-align:center;
+            }
+
+            .hero {
+                padding:100px 20px;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <div class="hero">
+            <h2>Danilo Bonometti</h2>
+            <h1>Thriller ad alta tensione tra montagne e missioni estreme</h1>
+        </div>
+
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     import os
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-
