31fe271a5a67a1235ad62a28a9a1ff95d5351977
MacBook nuovo Dad <macbooknuovodad@MacBook-Air-di-MacBook.local>
2026-03-22 17:18:45 +0100

    fix copertine corrette

--- app.py
+++ app.py
@@ -87,17 +87,17 @@ def home():
 
             <div class="books">
 
-                <!-- QUOTA 3000 PRIMO -->
+                <!-- QUOTA 3000 (CORRETTO) -->
                 <div class="book">
-                    <img src="https://i.imgur.com/LLk2xv1.jpeg">
+                    <img src="https://i.imgur.com/C8pXDwC.jpeg">
                     <h2>Quota 3000 – Scontro fra le rocce</h2>
                     <p>Un thriller ad alta tensione tra le montagne, dove ogni passo può essere l’ultimo.</p>
                     <a href="https://amzn.eu/d/0dI3x2wD" target="_blank" class="btn">Acquista su Amazon</a>
                 </div>
 
-                <!-- SEQUEL -->
+                <!-- L’OMBRA DEL PREDATORE (CORRETTO) -->
                 <div class="book">
-                    <img src="https://i.imgur.com/C8pXDwC.jpeg">
+                    <img src="https://i.imgur.com/LLk2xv1.jpeg">
                     <h2>L’ombra del predatore</h2>
                     <p>Il sequel ancora più oscuro, dove il pericolo non si vede… ma è sempre presente.</p>
                     <a href="https://amzn.eu/d/01ST1DQa" target="_blank" class="btn">Acquista su Amazon</a>
